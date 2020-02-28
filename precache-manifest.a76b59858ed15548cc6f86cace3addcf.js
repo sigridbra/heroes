ebe1a34655d52b4f3d30cb88543c4c93dd2d3487
Sigrid <sigbrats@gmail.com>
2020-02-28 09:02:35 +0100

    Updates

---
+++ precache-manifest.a76b59858ed15548cc6f86cace3addcf.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fd50a48429602fd90b544153b93bc2e8",
+    "url": "/heroes/index.html"
+  },
+  {
+    "revision": "daa8f481f0c4f3bf720e",
+    "url": "/heroes/static/css/main.e5f828c7.chunk.css"
+  },
+  {
+    "revision": "9c64da066188fe4fb23d",
+    "url": "/heroes/static/js/2.f70bfb46.chunk.js"
+  },
+  {
+    "revision": "443bf7e9a1e9cb088c914209dc5bf0a1",
+    "url": "/heroes/static/js/2.f70bfb46.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "daa8f481f0c4f3bf720e",
+    "url": "/heroes/static/js/main.b8d645cb.chunk.js"
+  },
+  {
+    "revision": "f30bb49aec1c65932f1c",
+    "url": "/heroes/static/js/runtime-main.5f151e31.js"
+  },
+  {
+    "revision": "49e28de89d5a2cc4d5fc615e509d1aae",
+    "url": "/heroes/static/media/gb.49e28de8.svg"
+  },
+  {
+    "revision": "4b57a28e497b81a0fb3063015dfbe800",
+    "url": "/heroes/static/media/no.4b57a28e.svg"
+  }
+]);
